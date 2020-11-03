c6f5b6639653e923eafaa0262d6ec8d729e0b821
Sandesh Chhetry <67684192+sandesh-chhetry@users.noreply.github.com>
2020-11-03 22:23:04 +0545

    assignment first

---
+++ Python Assignment 1/timeConverter.py
@@ -0,0 +1,10 @@
+##Exercise 3. Write a program that takes seconds as time units and converts it to minutes and
+##seconds.
+##For example: 3800 seconds results in 63 minutes and 20 seconds.
+
+getSecond = input("insert time in second: ")
+getMin = int(getSecond)//60
+getSec = int(getSecond)%60
+
+##there was an error to concadinate string with other datatype so i convert other data type to string for better output
+print(str(getSecond) + " Seconds = "+ str(getMin) + " Minute "+ str(getSec) + " Second" )
---
+++ Python Assignment 1/type.py
@@ -0,0 +1,27 @@
+##Exercise 1. Assume that we execute the following assignment statements:
+##width = 17
+##height = 12.0
+##delimiter = '.'
+##For each of the following expressions, write the value and the type of the expression.
+##1. width/2
+##2. width/2.0
+##3. height/3
+##4. 1 + 2 * 5
+##5. delimiter * 5
+
+width = 17
+height = 12.0
+delimiter = '.'
+
+##there was an error to concadinate string with other datatype so i convert other data type to string for better output
+print(width/2)
+print("type of width/2 is: "+ str(type(width/2)))
+print(width/2.0)
+print("type of width/2.0 is: "+ str(type(width/2.0)))
+print(height/3)
+print("type of height/3 is: "+ str(type(height/3)))
+print(1+2*5)
+print("type of 1+2*5 is: "+ str(type(1+2*5)))
+print(delimeter*5)
+print("type of delimeter*5 is: "+ str(type(delimeter*5)))
+type(delimeter*5)
